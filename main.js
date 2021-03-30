ca0c5fba770d38b841af23f98121c3f7c6834b88
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-03-30 18:21:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 function preload(){
 
 };
-function setup(){
-canvas=createCanvas(250 ,250);
-canvas.position(660,450)
-video=createCapture(VIDEO);
-video.hide();
-console.log(ml5.version);   
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0k1cklpzQ/model.json",modelloaded);
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(690 ,360);
+  video = createCapture(VIDEO);
+  video.hide();
+  console.log(ml5.version);
+  classifier=ml5.imageClassifier("MobileNet",modelLoaded);
 };
+
 function draw(){
-image(video,0,0,250,250) ;
-classifier.classify(video,gotResult);
+image(video,0,0,300,300);
+classifier.classify(video,gotresult);
 };
-function modelloaded(){
-console.log("model is loaded");
+function modelLoaded(){
+console.log("model is loaded !");
 };
-function gotResult(error,result){
-if(error){
-    console.log(error);
+function gotresult(error,result){
+if (error){
+  console.log(error);
 }
-else{
-    console.log(result);
-    document.getElementById("object").innerHTML= "Object -  " + result[0].label;
-    document.getElementById("acuracy").innerHTML= "Acuracy -  " + result[0].confidence.toFixed(3);
+else {
+  console.log(result);
+  document.getElementById("Object").innerHTML=result[0].label;
+  document.getElementById("confidence").innerHTML=result[0].confidence;
 };
 };
+
+
